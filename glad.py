0dbef9dcd357ce1a6c449491da6bf9180201852b
kevinjangwang <kevinjangwang@gmail.com>
2024-10-04 15:02:25 +0200

    Version 1 test
    Gladiator

---
+++ glad.py
@@ -0,0 +1,11 @@
+import random
+spelare_hälsa = 30 
+Gladiators_hälsa = 30 
+
+while spelare_hälsa > 0 and Gladiators_hälsa > 0:
+    print("Du vaknar upp i ett rum fullt med människor, en kille med en rödd tygg dräkt tittar glor ner på dig")
+    namn = input("Första gången här? Vad heter du?")
+    print(f"Välkommen till Rom {namn}, du är på ett väldigt tight situation just nu.")
+    print(" Du ser, du är en fångad med dem andra här och ska slåss till dödens om tjejsaren inte säger det själv")
+    print("Människorna här kallar oss för gladiator. Vi slåss för våran frihet eller ja, försöker iallafal.")
+    print("Jag tror att din första match är snart framme. *Han pekar på ett kort svärd som ligger på golvet* Öva med den så kommer du säkert klara dig.")
